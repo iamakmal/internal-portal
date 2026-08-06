86fc060c147874b2832abb44434d684be8dfa17d
Akmal Alavi <95517227+iamakmal@users.noreply.github.com>
2026-08-06 14:15:10 +0530

    fix: db error

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -13,6 +13,9 @@ function createPrismaClient() {
     password: process.env.DATABASE_PASSWORD,
     database: process.env.DATABASE_NAME,
     connectionLimit: 5,
+    ssl:{
+      rejectUnauthorized: true
+    }
   });
 
   return new PrismaClient({ adapter });
